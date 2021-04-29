e3921dc7e39e9de12133b3ab5abfa4979436462e
Damián <dsthode@gmail.com>
2021-04-29 21:41:00 +0200

    primer commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,14 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+import * as bootstrap from 'bootstrap';
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
